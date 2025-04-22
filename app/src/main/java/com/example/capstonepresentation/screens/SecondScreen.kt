a4f7621e26d251a25f3ff6e1f596ae76a1cba039
Jack <huanj102@mcmaster.ca>
2025-04-21 21:05:59 -0400

    Files added

---
+++ app/src/main/java/com/example/capstonepresentation/screens/SecondScreen.kt
@@ -0,0 +1,9 @@
+package com.example.capstonepresentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SecondScreen(navController: NavController) {
+
+}
