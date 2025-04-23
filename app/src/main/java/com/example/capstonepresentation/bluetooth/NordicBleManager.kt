37043938248d5c3afe86a9065ae57212e0b68fd8
Jack <huanj102@mcmaster.ca>
2025-04-23 19:06:46 -0400

    Overwrite BleManager from Nordic library

---
+++ app/src/main/java/com/example/capstonepresentation/bluetooth/NordicBleManager.kt
@@ -0,0 +1,47 @@
+package com.example.capstonepresentation.bluetooth
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import no.nordicsemi.android.ble.BleManager
+import no.nordicsemi.android.ble.ktx.state.ConnectionState
+import no.nordicsemi.android.ble.ktx.suspend
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import com.example.capstonepresentation.bluetooth.BluetoothConstants
+import java.util.UUID
+
+@Singleton
+class NordicBleManager @Inject constructor(
+    @ApplicationContext context: Context
+) : BleManager(context) {
+
+    private val targetServiceUuid: UUID? = BluetoothConstants.UART_SERVICE_UUID
+    private val targetCharUuid: UUID? = BluetoothConstants.UART_RX_CHAR_UUID
+
+    private val _rawData = MutableSharedFlow<ByteArray>(
+        replay = 0,
+        extraBufferCapacity = 10,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+
+    val rawData: SharedFlow<ByteArray> = _rawData
+
+    override fun getGattCallback(): BleManagerGattCallback {
+        return object : BleManagerGattCallback() {
+            override fun initialize() {
+
+                setNotificationCallback(uartCharacteristic)
+                    .with { _, data ->
+                        _rawData.tryEmit(data.value)
+                    }
+            }
+        }
+    }
+
+
+}
